f72bde6433a749b61937099ed3ace7d28764f4fe
coyamasss <coyamasss@00f2200d-f7a8-dc77-c299-15437ac5ebba>
2013-06-15 01:23:15 +0000

    FIX ProjectSmart.bat

--- Tips/ProjectSmart.bat
+++ Tips/ProjectSmart.bat
@@ -14,21 +14,21 @@ goto EXT
 
 rem Delete for Folder and object
 :KILL
-for /r %%A in ( Debug ) do ( if exist "%%A" ( rmdir /s /q "%%A"))
-for /r %%B in ( debug ) do ( if exist "%%B" ( rmdir /s /q "%%B"))
-for /r %%C in ( Release ) do ( if exist "%%C" ( rmdir /s /q "%%C"))
-for /r %%D in ( release ) do ( if exist "%%D" ( rmdir /s /q "%%D"))
-for /r %%E in ( ipch ) do ( if exist "%%E" ( rmdir /s /q "%%E"))
-for /r %%F in ( obj ) do ( if exist "%%F" ( rmdir /s /q "%%F"))
-for /r %%G in ( *.suo ) do ( if exist "%%G" ( del /s /q "%%G"))
-for /r %%H in ( *.user ) do ( if exist "%%H" ( del /s /q "%%H"))
-for /r %%I in ( *.sdf ) do ( if exist "%%I" ( del /s /q "%%I"))
-for /r %%J in ( *.bak ) do ( if exist "%%J" ( del /s /q "%%J"))
-for /r %%K in ( *.vcproj ) do ( if exist "%%K" ( del /s /q "%%K"))
-for /r %%M in ( _UpgradeReport_File ) do ( if exist "%%M" ( rmdir /s /q "%%M"))
-for /r %%N in ( *.ncb ) do ( if exist "%%N" ( del /s /q "%%N"))
-for /r %%L in ( *.opensdf ) do ( if exist "%%L" ( del /s /q "%%L"))
-for /r %%O in ( Thumbs.db ) do ( if exist "%%O" ( del /s /q "%%O"))
+rem Debug or debug
+for /r %%A in ( debug ) do ( if exist "%%A" ( rmdir /s /q "%%A"))
+for /r %%B in ( release ) do ( if exist "%%B" ( rmdir /s /q "%%B"))
+for /r %%C in ( ipch ) do ( if exist "%%C" ( rmdir /s /q "%%C"))
+for /r %%D in ( obj ) do ( if exist "%%D" ( rmdir /s /q "%%D"))
+for /r %%E in ( *.suo ) do ( if exist "%%E" ( del /s /q "%%E"))
+for /r %%F in ( *.user ) do ( if exist "%%F" ( del /s /q "%%F"))
+for /r %%G in ( *.sdf ) do ( if exist "%%G" ( del /s /q "%%G"))
+for /r %%H in ( *.bak ) do ( if exist "%%H" ( del /s /q "%%H"))
+for /r %%I in ( *.vcproj ) do ( if exist "%%I" ( del /s /q "%%I"))
+for /r %%J in ( _UpgradeReport_File ) do ( if exist "%%J" ( rmdir /s /q "%%J"))
+for /r %%L in ( *.ncb ) do ( if exist "%%L" ( del /s /q "%%L"))
+for /r %%M in ( *.opensdf ) do ( if exist "%%M" ( del /s /q "%%M"))
+for /r %%N in ( Thumbs.db ) do ( if exist "%%N" ( del /s /q "%%N"))
+for /r %%O in ( *~ ) do ( if exist "%%O" ( del /s /q "%%O"))
 
 goto EXT
 
