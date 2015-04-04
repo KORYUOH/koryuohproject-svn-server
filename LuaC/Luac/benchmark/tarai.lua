bf422166b99203f45dd179905c18dd3b027d17d6
KORYUOH <coyamasss@yahoo.co.jp>
2015-04-04 17:06:41 +0900

    sub moduling

--- LuaC/Luac/benchmark/tarai.lua
+++
@@ -1,8 +0,0 @@
-function tarai(x, y, z)
-  if x <= y then
-    return y
-  else
-    return tarai(tarai(x - 1, y, z), tarai(y - 1, z, x), tarai(z - 1, x, y))
-  end
-end
-print (tarai( 15 , 4 , 0 ))
